27d67c076df65135b1532958b14fdc8d7ef90473
hanksterhan <henryhan62@gmail.com>
2019-03-08 12:55:58 +0100

    finished circle geometry

--- js/CircleGeometry.js
+++ js/CircleGeometry.js
@@ -1,39 +1,52 @@
+// Created by Henry Han
 "use strict";
 const CircleGeometry = function(gl) {
   this.gl = gl;
+  
+  this.vertices = [0,0,0]; 
+  var angle = 0
+  for(var i=0; i<=64; i++){
+      this.vertices.push(Math.cos(angle));
+      this.vertices.push(Math.sin(angle));
+      this.vertices.push(0.5);
+      angle += (2*Math.PI) / 64;
+      console.log(Math.cos(angle));
+      console.log(Math.sin(angle));
+    }
+
+  this.colors = [0,0,0]; 
+  for(var i=0; i<=64; i++){
+    this.colors.push(0.3);
+    this.colors.push(0.2);
+    this.colors.push(0.7);
+  }
+
+  this.triplets = []; // vertices of triangles to draw
+  for(var i=1; i<=64; i++){
+    this.triplets.push(i);
+    this.triplets.push(i+1);
+    this.triplets.push(0);
+  }
 
   // allocate and fill vertex buffer in device memory (OpenGL name: array buffer)
   this.vertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
   gl.bufferData(gl.ARRAY_BUFFER,
-    new Float32Array([
-      -0.5, -0.5, 0.5,
-      -0.5,  0.5, 0.5,
-       0.5,  0.5, 0.5,
-       0.5, -0.5, 0.5,
-    ]),
+    new Float32Array(this.vertices),
     gl.STATIC_DRAW);
 
   // allocate and fill vertex buffer in device memory (OpenGL name: array buffer)
   this.colorBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
   gl.bufferData(gl.ARRAY_BUFFER,
-    new Float32Array([
-       0.7,  0.2, 0.5,
-       0.2,  0.4, 0.5,
-       0.9,  0.3, 0.5,
-       0.3,  0.4, 0.1, 
-      ]),
+    new Float32Array(this.colors),
     gl.STATIC_DRAW);
 
   // allocate and fill index buffer in device memory (OpenGL name: element array buffer)
   this.indexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
-    new Uint16Array([
-      0, 1, 3,
-      1, 2, 3,
-    ]),
+    new Uint16Array(this.triplets),
     gl.STATIC_DRAW);
 
   // create and bind input layout with input buffer bindings (OpenGL name: vertex array)
@@ -68,5 +81,5 @@ CircleGeometry.prototype.draw = function() {
   gl.bindVertexArray(this.inputLayout);
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);  
 
-  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+  gl.drawElements(gl.TRIANGLES, 192, gl.UNSIGNED_SHORT, 0);
 };
